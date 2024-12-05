b822d1ebc744d4c0b75cde38e73825414aa7ba35
Aavtic <aadishm79@gmail.com>
2024-12-05 23:57:52 +0530

    Added Status code

--- poller.rs
+++ poller.rs
@@ -4,10 +4,15 @@ use std::sync::mpsc::{channel, Sender};
 use std::process::Command;
 use std::process::Stdio;
 
+pub enum StdinDataStatus {
+    Data(String),
+    Over(i32),
+}
+
 pub enum StdinData {
-    StdinSender(Sender<String>),
+    StdinSender(Sender<StdinDataStatus>),
     Available(u8),
-    Over,
+    Over(i32),
 }
 
 
@@ -20,7 +25,7 @@ pub fn read_line_stdin() -> io::Result<()> {
 }
 
 pub fn live_read_stdin(program: String, arguments: Vec<String>, sender: Sender<StdinData>) -> io::Result<()>{
-    let (tx, rx) = channel::<String>();
+    let (tx, rx) = channel::<StdinDataStatus>();
 
     sender.send(StdinData::StdinSender(tx.clone())).unwrap();
 
@@ -38,14 +43,16 @@ pub fn live_read_stdin(program: String, arguments: Vec<String>, sender: Sender<S
     std::thread::spawn(move || {
         loop {
             match rx.recv() {
-                Ok(stdin_str) => {
+                Ok(StdinDataStatus::Data(stdin_str)) => {
                     if stdin.write_all(stdin_str.as_bytes()).is_err() {
-                        sender_clone.send(StdinData::Over).unwrap();
+                        // TODO!
+                        // Temporary Fix below
+                        let _ = sender_clone.send(StdinData::Over(-1));
                         return;
                     };
                 },
-                Err(_e) => {
-                }
+                Ok(StdinDataStatus::Over(_code)) => return,
+                Err(_e) => return,
             }
         }
     });
@@ -58,16 +65,31 @@ pub fn live_read_stdin(program: String, arguments: Vec<String>, sender: Sender<S
                 println!("sending over");
                 // TODO!
                 // Temporary Fix below;
-                if sender.send(StdinData::Over).is_err() {
+                let status = proc.wait().unwrap();
+                if let Some(code) = status.code() {
+                    if sender.send(StdinData::Over(code)).is_err() {
+                        return Ok(());
+                    };
+                }
+                if sender.send(StdinData::Over(-1)).is_err() {
                     return Ok(());
                 };
                 return Ok(());
             },
             Ok(_) => {
+                print!("got data: {:?}", buffer[0]);
                 sender.send(StdinData::Available(buffer[0])).unwrap(); 
             },
             Err(_) => {
-                sender.send(StdinData::Over).unwrap();
+                let status = proc.wait().unwrap();
+                if let Some(code) = status.code() {
+                    if sender.send(StdinData::Over(code)).is_err() {
+                        return Ok(());
+                    };
+                }
+                if sender.send(StdinData::Over(-1)).is_err() {
+                    return Ok(());
+                };
                 return Ok(());
             }
         }
